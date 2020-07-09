6fcc9e963abcb850a724a22119d066a22cc73368
flytr4p42 <67969563+flytr4p42@users.noreply.github.com>
2020-07-09 10:40:24 +0100

    Add files via upload

---
+++ JS/fizzbuzz.js
@@ -0,0 +1,23 @@
+let num = 61
+let fizz = (num % 3);
+let buzz = (num % 5);
+let fizzBuzz = (num % 15);
+
+if ((fizzBuzz == 0)){console.log("FIZZBUZZ")}
+else if (fizz == 0){console.log("FIZZ!!")}
+else if (buzz == 0){console.log("BUZZ!!")}
+else {console.log(num)};
+
+
+
+
+// switch (num) {
+//     case fizz == 0:
+//         console.log("FIZZZZZZZZZZZ")
+        
+//         break;
+
+//     default:
+//         console.log(num)
+//         break;
+// }
